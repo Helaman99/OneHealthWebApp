aac987d39b1288f7771970a7080e34236dda42a7
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2025-01-15 19:49:53 -0800

    Added navigation links to the dashboard

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,5 +1,17 @@
+import {Link } from 'react-router-dom';
+import { useAppContext } from '../contexts/userContexts';
+
 export default function Dashboard() {
+    const { sharedValue } = useAppContext();
     return(
-        <p>This is the dashboard!</p>
+        <>
+            <p>Welcome to your dashboard, { sharedValue }!</p>
+            <ul>
+                <li><Link to='/medication'>Your Medication</Link></li>
+                <li><Link to='/rewards'>Your Rewards</Link></li>
+                <li><Link to='/patent-page'>Patent Information</Link></li>
+                <li><Link to='/'>Logout</Link></li>
+            </ul>
+        </>
     );
 }
