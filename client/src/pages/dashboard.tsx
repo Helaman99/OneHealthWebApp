5a5dfd3e894a90e811ee781535c509d5add72ee5
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2025-01-22 13:49:37 -0800

    Building out profile settings page

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -5,12 +5,13 @@ export default function Dashboard() {
     const { sharedValue } = useAppContext();
     return(
         <>
-            <p>Welcome to your dashboard, { sharedValue }!</p>
+            <p>Welcome to your dashboard, { sharedValue.name }!</p>
             <ul>
                 <li><Link to='/medication'>Your Medication</Link></li>
                 <li><Link to='/rewards'>Your Rewards</Link></li>
                 <li><Link to='/patent-page'>Patent Information</Link></li>
                 <li><Link to='/profile'>Profile</Link></li>
+                <li><button>Notifications</button></li>
                 <li><Link to='/'>Logout</Link></li>
             </ul>
         </>
--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -1,12 +1,42 @@
 import {Link } from 'react-router-dom';
 import { useAppContext } from '../contexts/userContexts';
+import axios from 'axios';
+import { useState , useEffect} from 'react';
+
+interface RewardsHistory {
+    name: string;
+    points: number;
+    redeem_date: string;
+}
 
 export default function Profile() {
     const { sharedValue } = useAppContext();
+    const [rewardsWon, setRewardsWon] = useState<RewardsHistory[]>([]);
+    const [viewRewardsHistory, setViewRewardsHistory] = useState<boolean>(false);
+    var totalPoints = 0;
+
+    function ViewPastRewards(){
+        axios.get(`http://localhost:3000/${sharedValue.id}/rewards`)
+        .then((response) => {
+          console.log(response.data);
+          setRewardsWon(response.data.user);
+          setViewRewardsHistory(true);
+          for (var reward of rewardsWon) {
+            totalPoints += reward.points;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          console.log('No Rewards Redeemed');
+        });
+    }
+
     return(
         <>
-            <p>{ sharedValue }'s Profile!</p>
+            <p>{ sharedValue.name }'s Profile</p>
             <ul>
+                <li><p>Location: { sharedValue.location }</p></li>
+                <li><p>Earned Points: { totalPoints }</p></li>
                 <li><Link to='/dashboard'>Back</Link></li>
             </ul>
         </>
