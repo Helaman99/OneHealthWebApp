78e64e4736cceca29a3bb9afae75d0b78536d4e0
Helaman99 <112657994+Helaman99@users.noreply.github.com>
2025-01-28 14:31:16 -0800

    Update paths after merging main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes,Route } from 'react-router';
 import Rewards from './pages/rewards';
 import Home from './pages/home';
 import Dashboard from './pages/dashboard.tsx';
+import Profile from './pages/profile.tsx';
 import { BrowserRouter } from 'react-router';
 import { AppProvider } from './contexts/userContexts';
 import Navbar from './assets/navbar';
@@ -21,9 +22,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/rewards' element={<Rewards />} />
           <Route path='/learn' element= {<Learn />} />
           <Route path ='/medication' element={<Medication />} />
+          <Route path ='/profile' element={<Profile />} />
           <Route path ='/login' element={<Login />} />
           <Route path ='/register' element={<Register/>} />
           <Route path='/admin' element={<Admin /> } />
